44c08e7084a56fc83c9f1514f3edab12fdc1f200
diorgesherspt <diorgesherspt@outlook.com.br>
2022-06-20 15:27:27 -0300

    vai

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+const database = new Prohairesis(env.CLEARDB_DATABASE_URL)
+
 const PORT = process.env.PORT || 8877
 
 app.get('/sobre', (req,res)=>{
@@ -14,6 +16,21 @@ app.get('/', (req,res)=>{
     res.json({
         msg: "ok"
     })
+})
+
+app.get('/mensagem', async (req, res) => {
+    try{
+        const users =await database.query(
+            `select * from mensagem`
+        )
+        res.status(200)
+        res.json(users)
+    }
+    catch(err){
+        console.error('Erro usuarios')
+        res.status(500)
+        res.end('Erro ao procurar usuários')
+    }
 })
 
 app.listen(PORT, ()=>{
---
+++ env.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+
+module.exports = {
+    CLEARDB_DATABASE_URL:"mysql://b1a62c461dee8c:26071eec@us-cdbr-east-05.cleardb.net/heroku_7b5582f5c601ee7?reconnect=true"}
+
+
+    
